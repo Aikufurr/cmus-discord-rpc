a64cf84a0d821c58487486ba4eaedd26feeb40a9
Aikufurr <ikeybill1@gmail.com>
2022-03-17 22:20:20 +0000

    Updated source code
    Updated to nickofolas's Discord RPC library
    Added code to host album covers and buttons for my Lapfox music library

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ use std::str::FromStr;
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-use discord_rpc_client::Client;
-use discord_rpc_client::models::Activity;
+use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
 
 use env_logger;
 use log::{debug, info};
@@ -42,7 +41,7 @@ impl FromStr for Status {
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     info!("Starting cmus-discord-rpc...");
@@ -50,16 +49,24 @@ fn main() {
     let socket_path = get_socket_path();
     debug!("Using cmus socket {}", socket_path);
     let mut stream = get_unix_stream(&socket_path);
-    let mut drpc = Client::new(431179120836214795);
-    drpc.start();
+    let mut drpc = DiscordIpcClient::new("953758667369513040")?;
+    drpc.connect()?;
+    let mut connected = true;
 
     let mut output = String::new();
 
     loop {
         if stream.write_all(b"status\n").is_err() {
-            drpc.clear_activity().expect("Failed to clear presence");
+            drpc.close()?;
+            connected = false;
+            info!("Failed to clear presence");
             stream = get_unix_stream(&socket_path);
             continue;
+        } else {
+            if !connected {
+                drpc.reconnect()?;
+                connected = true
+            }
         }
 
         let mut reader = BufReader::new(&stream);
@@ -70,8 +77,10 @@ fn main() {
         debug!("Received\n{}", output);
 
         let status = get_value(&output, "status").unwrap().parse::<Status>().unwrap();
-        let mut ac = Activity::new()
-                        .details(status.to_string());
+        let mut ac = activity::Activity::new();
+        let mut url_string: String;
+        let mut assets = activity::Assets::new();
+        let album_url: String;
         if status != Status::Stopped {
             let artist = get_value(&output, "tag artist");
             let title = get_value(&output, "tag title");
@@ -83,22 +92,68 @@ fn main() {
                     Some(v) => ac = ac.state(v.get(1).unwrap().as_str()),
                     None => ac = ac.state("")
                 }
+            } else {
+                ac = ac.details(title.unwrap());
+                ac = ac.state(artist.unwrap());
             }
-            else {
-                ac = ac.state(artist.unwrap().to_owned() + " - " + title.unwrap());
+
+            // -- Album cover --
+            // Capture folder of artist and folder
+            let file_r = Regex::new(r"(?m)^file *.+/(.+)/(.+)/(.+)/").unwrap();
+            let base_folder: &str;
+            let artist_folder: &str;
+            let album_folder: &str;
+            match file_r.captures(&output) {
+                Some(v) => {base_folder = v.get(1).unwrap().as_str(); artist_folder = v.get(2).unwrap().as_str(); album_folder = v.get(3).unwrap().as_str()},
+                None => {base_folder = ""; artist_folder = ""; album_folder = ""}
+            }
+
+            if !(artist_folder.is_empty() || album_folder.is_empty()) {
+                url_string = format!("https://aikufurr.xyz/presence?{}cmus_artist={}&cmus_album={}", if base_folder.is_empty() {String::from("")} else {"cmus_base=".to_owned() + base_folder + "&"}, artist_folder, album_folder);
+                url_string = url_string.replace(" ", "%20");
+                let url: &str = &url_string;
+                assets = assets.large_image(url);
+                assets = assets.large_text(get_value(&output, "tag album").unwrap());
+                ac = ac.assets(assets);
+                if base_folder == "Lapfox" {
+                    let mut buttons: Vec<activity::Button> = vec![];
+
+                    let file_path: String = format!("/home/aikufurr/Music/Lapfox/{artist_folder}/{album_folder}/link.txt");
+                    if std::path::Path::new(&file_path).exists() {
+                        album_url = std::fs::read_to_string(file_path)?.parse()?;
+                        let stripped_url: &str;
+                        match album_url.lines().next() {
+                            Some(v) => stripped_url = v,
+                            None => stripped_url = ""
+                        }
+                        if !stripped_url.is_empty() {
+                            buttons.push(activity::Button::new("View Album", stripped_url));
+                        }
+                    }
+
+                    buttons.push(activity::Button::new("Halley Labs", "https://halleylabs.com/"));
+
+                    ac = ac.buttons(buttons);
+                }
             }
 
             if status == Status::Playing {
                 let duration = get_value(&output, "duration").unwrap().parse::<u64>().unwrap();
                 let position = get_value(&output, "position").unwrap().parse::<u64>().unwrap();
                 let sce = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-                ac = ac.timestamps(|t| t.end(sce + duration - position));
+                let num:u64 = sce + duration - position;
+                let ts = activity::Timestamps::new().end(num as i64);
+                ac = ac.timestamps(ts);
             }
+        } else {
+            assets = assets.large_image("https://aikufurr.xyz/presence?cmus_artist=cmus&cmus_album=cmus");
+            assets = assets.large_text("C* Music Player (cmus)");
+            ac = ac.assets(assets);
         }
 
-        drpc.set_activity(|_| ac).expect("Failed to set presence");
+        drpc.set_activity(ac).expect("Failed to set presence");
 
-        thread::sleep(Duration::from_secs(15));
+        thread::sleep(Duration::from_secs(5));
     }
 }
 
